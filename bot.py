66db30a005ddb65fac4024e26c7a9a555ba8960c
Andrey Kozachek <xtreezzz@gmail.com>
2018-09-08 18:17:04 +0300

    bot

---
+++ bot.py
@@ -0,0 +1,36 @@
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import logging
+
+logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO,
+                    filename='bot2.log'
+                    )
+
+
+PROXY = {'proxy_url': 'socks5://t1.learn.python.ru:1080',
+    'urllib3_proxy_kwargs': {'username': 'learn', 'password': 'python'}}
+
+
+
+
+def greet_user(bot, update):
+    text = f'Вызван /start{update}'
+    update.message.reply_text(text)
+
+
+def talk_to_me(bot, update):
+    user_text = update.message.text
+    print(user_text)
+    update.message.reply_text(user_text)
+
+
+def main():
+    mybot = Updater('482892006:AAHMhN6da43e_4UDCgA4T5BKY2OgZ2BcF3I', request_kwargs=PROXY)
+
+    dp = mybot.dispatcher
+    dp.add_handler(CommandHandler("start", greet_user))
+    dp.add_handler(MessageHandler(Filters.text, talk_to_me))
+    mybot.start_polling()
+    mybot.idle()
+
+main()
