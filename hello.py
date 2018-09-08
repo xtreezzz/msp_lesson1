eb6ed5641dc8c395ea6c65cb20550d03ba949350
Андрей Козачек <xtreezzz@gmail.com>
2018-09-08 16:43:51 +0300

    Initial commit

---
+++ hello.py
@@ -0,0 +1,4 @@
+def get_summ(one, two, delimeter=' '):
+    return str(one).upper() + str(delimeter) + str(two).upper()
+x = get_summ('learn','python')
+print (x)
---
+++ info.py
@@ -0,0 +1,4 @@
+def format_price(price):
+	return f'Цена: {int(price)} руб'
+display_price =format_price(56.24)
+print (display_price)
